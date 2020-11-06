1906121bc6ec36253f8e8e8e989d213a03af4cf1
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-11-06 08:21:39 +0200

    Test recursion

--- tmc/reflect.py
+++ tmc/reflect.py
@@ -1,4 +1,38 @@
 import importlib
+from bdb import Bdb
+import sys
+
+class RecursionDetected(Exception):
+    pass
+
+class RecursionDetector(Bdb):
+    def do_clear(self, arg):
+        pass
+
+    def __init__(self, *args):
+        Bdb.__init__(self, *args)
+        self.stack = set()
+
+    def user_call(self, frame, argument_list):
+        code = frame.f_code
+        if code in self.stack:
+            raise RecursionDetected
+        self.stack.add(code)
+
+    def user_return(self, frame, return_value):
+        self.stack.remove(frame.f_code)
+
+def test_recursion(func: callable, *args):
+    detector = RecursionDetector()
+    detector.set_trace()
+    try:
+        func(*args)
+    except RecursionDetected:
+        return True
+    else:
+        return False
+    finally:
+        sys.settrace(None)
 
 class Reflect:
     def __init__(self, modulename:str = "", classname:str = ""):
